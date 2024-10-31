d23a646a4ebad4ba02cc777d6da862684ff89345
Anupam Ahi <anupam.gollapalli@gmail.com>
2024-10-30 22:07:13 -0700

    added client and server

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -1,12 +1,28 @@
 package org.example;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
 public class Client {
-    public static void main(String[] args)  throws IOException {
-        Socket client = new Socket("localhost", 8080);
-
+    private Socket client;
+    private DataOutputStream out;
+    public Client() {
+        try{
+            client = new Socket("localhost", Server.PORT);
+            out = new DataOutputStream(client.getOutputStream());
+            writeMessages();
+        }catch (IOException e){}
     }
-
+    public void writeMessages() throws IOException {
+        String message = "Hello, I am Anupam. ";
+        out.writeUTF(message);
+    }
+    private void close() throws IOException {
+        client.close();
+        out.close();
+    }
+    public static void main(String[] args)  throws IOException {
+        new Client();
+    }
 }
--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -1,10 +1,41 @@
 package org.example;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
+        private final ServerSocket server;
+        private DataInputStream in;
+        static final int PORT = 8080;
+        public Server() throws IOException {
+            server = new ServerSocket(PORT);
+            initializeConnection();
+        }
+        private void initializeConnection() throws IOException {
+            Socket clientSocket = server.accept();
+            in = new DataInputStream(new BufferedInputStream(clientSocket.getInputStream()));
+            readClientMessages();
+            closeConnection();
+        }
+        private void readClientMessages() throws IOException {
+            String line;
+            while ((line = in.readUTF()) != null) {
+                line = line.trim();
+                System.out.println(line);
+            }
+        }
+        private void closeConnection() throws IOException {
+            if(in != null) {
+                in.close();
+            }
+            if(server != null && !server.isClosed()) {
+                server.close();
+            }
+        }
     public static void main(String[] args) throws IOException {
-
-
+            new Server();
     }
 }
